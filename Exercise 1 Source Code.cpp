76b9f97efea432fec062ae5e2b713f68d987c4d0
BlackYagami <117858598+BlackYagami@users.noreply.github.com>
2022-11-09 14:23:28 -0500

    Delete Exercise 1 Source Code.cpp

--- Exercise 1 Source Code.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() //no need to put ; after parameter because function body follow parameter 
-{
-	std::cout << "Hello, programming!\n"; 
-	std::cout << "Here we go!\n";
-	(void)getchar(); // void function return type must be put in paranthesis to explicitly ignore return value that has no parameter attached to getchar
-		return 0; 
-}
